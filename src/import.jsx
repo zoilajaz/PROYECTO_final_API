b0b59b83cc56b261d0fd40bae695b216f690a26e
zoilajaz <l19350292@tuxtepec.tecnm.mx>
2023-06-06 15:47:53 -0500

    proyecto_api

---
+++ src/import.jsx
@@ -0,0 +1,53 @@
+const images = [
+  {
+    src:"https://http.dog/100.jpg",
+    player: '100'
+  },
+  {
+    src:"https://http.dog/100.jpg",
+    player: '100'
+  },
+  {
+    src:"https://http.dog/101.jpg",
+    player: '101'
+  },
+  {
+    src:"https://http.dog/101.jpg",
+    player: '101'
+  },
+  {
+    src:"https://http.dog/102.jpg",
+    player: '102'
+  },
+  {
+    src:"https://http.dog/102.jpg",
+    player: '102'
+  },
+  {
+    src:"https://http.dog/103.jpg",
+    player: '103'
+  },
+  {
+    src:"https://http.dog/103.jpg",
+    player: '103'
+  },
+  {
+    src:"https://http.dog/200.jpg",
+    player: '200'
+  },
+  {
+    src:"https://http.dog/200.jpg",
+    player: '200'
+  },
+  {
+    src: "https://http.dog/201.jpg",
+    player: '201'
+  },
+  {
+    src:"https://http.dog/201.jpg",
+    player: '201'
+  },
+
+];
+
+export { images } 
